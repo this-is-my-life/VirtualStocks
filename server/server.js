0aae377e836027e13d891da649a54a22e3bdab40
PMHStudio <pmhstudio.pmh@gmail.com>
2019-04-06 11:31:44 +0900

    Open Beta Released!

--- server/server.js
+++ server/server.js
@@ -1,11 +1,110 @@
 const superagent = require('superagent')
+let obt1HackBlock
+superagent.get('https://api.jsonbin.io/b/5ca80c270f4c9334823b59dc/latest').then((response) => {
+  obt1HackBlock = response
+})
 
-processStart()
+cbtServer()
+obt1Server()
+
 setInterval(() => {
-  processStart()
+  cbtServer()
+  obt1Server()
 }, process.env.cycleDelay * 1000);
 
-function processStart () {
+function obt1Server () {
+  superagent.get('https://api.jsonbin.io/b/5ca80c270f4c9334823b59dc/latest').then((response) => {
+    obt1HackBlock = response
+    obt1HackBlock.body.info.routine++
+    let last = new Date()
+    let next = new Date()
+    obt1HackBlock.body.info.lastUpdate = last
+    next.setTime(last.getTime() + (process.env.cycleDelay * 1000))
+    obt1HackBlock.body.info.nextUpdate = next
+    for (let counter = 0; counter < obt1HackBlock.body.chart.length; counter++) {
+      obt1HackBlock.body.chart[counter].before = obt1HackBlock.body.chart[counter].after
+      if (obt1HackBlock.body.chart[counter].name === 'HC주식회사') {
+        let random1 = getRandomInt(45, 58)
+        let random2 = getRandomInt(1, 101)
+        let random3 = getRandomInt(1, 41)
+        if (random2 <= random1) {
+          obt1HackBlock.body.chart[counter].after += random3
+        } else {
+          obt1HackBlock.body.chart[counter].after -= random3
+        }
+        if (obt1HackBlock.body.chart[counter].after <= 0) {
+          obt1HackBlock.body.chart[counter].after = obt1HackBlock.body.chart[counter].before
+        }
+      } else if (obt1HackBlock.body.chart[counter].name === '뮤트테크') {
+        let random1 = getRandomInt(44, 57)
+        let random2 = getRandomInt(1, 101)
+        let random3 = getRandomInt(1, 33)
+        if (random2 <= random1) {
+          obt1HackBlock.body.chart[counter].after += random3
+        } else {
+          obt1HackBlock.body.chart[counter].after -= random3
+        }
+        if (obt1HackBlock.body.chart[counter].after <= 0) {
+          obt1HackBlock.body.chart[counter].after = obt1HackBlock.body.chart[counter].before
+        }
+      } else if (obt1HackBlock.body.chart[counter].name === 'TK전자') {
+        let random1 = getRandomInt(35, 45)
+        let random2 = getRandomInt(1, 101)
+        let random3 = getRandomInt(1, 101)
+        if (random2 <= random1) {
+          obt1HackBlock.body.chart[counter].after += random3
+        } else {
+          obt1HackBlock.body.chart[counter].after -= random3
+        }
+        if (obt1HackBlock.body.chart[counter].after <= 0) {
+          obt1HackBlock.body.chart[counter].after = obt1HackBlock.body.chart[counter].before
+        }
+      } else if (obt1HackBlock.body.chart[counter].name === 'PC가전') {
+        let random1 = getRandomInt(43, 55)
+        let random2 = getRandomInt(1, 101)
+        let random3 = getRandomInt(1, 26)
+        if (random2 <= random1) {
+          obt1HackBlock.body.chart[counter].after += random3
+        } else {
+          obt1HackBlock.body.chart[counter].after -= random3
+        }
+        if (obt1HackBlock.body.chart[counter].after <= 0) {
+          obt1HackBlock.body.chart[counter].after = obt1HackBlock.body.chart[counter].before
+        }
+      } else if (obt1HackBlock.body.chart[counter].name === '피엠산업') {
+        let random1 = getRandomInt(42, 53)
+        let random2 = getRandomInt(1, 101)
+        let random3 = getRandomInt(1, 21)
+        if (random2 <= random1) {
+          obt1HackBlock.body.chart[counter].after += random3
+        } else {
+          obt1HackBlock.body.chart[counter].after -= random3
+        }
+        if (obt1HackBlock.body.chart[counter].after <= 0) {
+          obt1HackBlock.body.chart[counter].after = obt1HackBlock.body.chart[counter].before
+        }
+      } else if (obt1HackBlock.body.chart[counter].name === '비빔밥사') {
+        let random1 = getRandomInt(41, 53)
+        let random2 = getRandomInt(1, 101)
+        let random3 = getRandomInt(1, 13)
+        if (random2 <= random1) {
+          obt1HackBlock.body.chart[counter].after += random3
+        } else {
+          obt1HackBlock.body.chart[counter].after -= random3
+        }
+        if (obt1HackBlock.body.chart[counter].after <= 0) {
+          obt1HackBlock.body.chart[counter].after = obt1HackBlock.body.chart[counter].before
+        }
+      }
+    }
+    console.log(obt1HackBlock.body)
+    superagent.put('https://api.jsonbin.io/b/5ca80c270f4c9334823b59dc').send(obt1HackBlock.body).catch((err) => { console.log(err) })
+  })
+  
+}
+
+function cbtServer () {
+  
   superagent.get('https://api.jsonbin.io/b/5c9cd7b9da12e364adfb7737/latest').then((response) => {
     response.body.info.routine++
     let last = new Date()
@@ -92,7 +191,6 @@ function processStart () {
     console.log(response.body)
     superagent.put('https://api.jsonbin.io/b/5c9cd7b9da12e364adfb7737').send(response.body).catch((err) => { console.log(err) })
   })
-  
 }
 
 function getRandomInt(min, max) {
